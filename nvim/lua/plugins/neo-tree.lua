063794cd0fdea468cc39d9cbb3e169db23ab89a7
mikael hallgren <hallgren.mikael@gmail.com>
2025-11-07 08:21:51 +0100

    dd multiline edit plugin

--- nvim/lua/plugins/neo-tree.lua
+++ nvim/lua/plugins/neo-tree.lua
@@ -1,25 +1,25 @@
 return {
-	"nvim-neo-tree/neo-tree.nvim",
-	branch = "v3.x",
-	dependencies = {
-		"nvim-lua/plenary.nvim",
-		"nvim-tree/nvim-web-devicons",
-		"MunifTanjim/nui.nvim",
-	},
-	config = function()
-		require("neo-tree").setup({
-			filesystem = {
-				filtered_items = {
-					visible = true, -- This is what you want: If you set this to `true`, all "hide" just mean "dimmed out"
-					hide_dotfiles = false,
-					hide_gitignored = false,
-					hide_by_name = {
-						"node_modules",
-					},
-				},
-			},
-		})
-		vim.keymap.set("n", "<C-n>", ":Neotree filesystem reveal left<CR>", {})
-		vim.keymap.set("n", "<leader>bf", ":Neotree buffers reveal float<CR>", {})
-	end,
+  "nvim-neo-tree/neo-tree.nvim",
+  branch = "v3.x",
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    "nvim-tree/nvim-web-devicons",
+    "MunifTanjim/nui.nvim",
+  },
+  config = function()
+    require("neo-tree").setup({
+      filesystem = {
+        filtered_items = {
+          visible = true, -- This is what you want: If you set this to `true`, all "hide" just mean "dimmed out"
+          hide_dotfiles = false,
+          hide_gitignored = false,
+          hide_by_name = {
+            "node_modules",
+          },
+        },
+      },
+    })
+    vim.keymap.set("n", "<C-b>", ":Neotree filesystem reveal left<CR>", {})
+    vim.keymap.set("n", "<leader>bf", ":Neotree buffers reveal float<CR>", {})
+  end,
 }
--- nvim/lua/plugins/none-ls.lua
+++ nvim/lua/plugins/none-ls.lua
@@ -1,52 +1,61 @@
 return {
-	{
-		"nvimtools/none-ls.nvim",
-		dependencies = { "nvimtools/none-ls-extras.nvim" },
-		config = function()
-			local null_ls = require("null-ls")
-			local utils = require("null-ls.utils")
+  {
+    "nvimtools/none-ls.nvim",
+    dependencies = { "nvimtools/none-ls-extras.nvim" },
+    config = function()
+      local null_ls = require("null-ls")
 
-			vim.api.nvim_create_autocmd("BufWritePre", {
-				callback = function()
-					vim.lsp.buf.format({ async = false })
-				end,
-			})
+      local utils = require("null-ls.utils")
 
-			local root =
-				utils.root_pattern(".git", "package.json", ".prettierrc", ".prettierrc.json")(vim.fn.expand("%:p"))
+      vim.api.nvim_create_autocmd("BufWritePre", {
+        callback = function()
+          vim.lsp.buf.format({ async = false })
+        end,
+      })
 
-			null_ls.setup({
-				root_dir = function()
-					return root
-				end,
-				sources = {
-					null_ls.builtins.formatting.prettier.with({
-						--	extra_args = { "--plugin-search-dir=" .. root },
+      local root =
+          utils.root_pattern(".git", "package.json", ".prettierrc", ".prettierrc.json")(vim.fn.expand("%:p"))
 
-						extra_args = { "--plugin-search-dir=." },
-						-- Optional: define the working directory (useful in monorepos)
-						cwd = function(params)
-							return utils.root_pattern(".git")(params.bufname)
-						end,
-						env = {
-							PRETTIER_LOG_LEVEL = "debug",
-						},
-					}),
-					require("none-ls.diagnostics.eslint_d"),
-				},
-			})
+      null_ls.setup({
+        root_dir = function()
+          return root
+        end,
+        sources = {
+          null_ls.builtins.formatting.prettier.with({
+            --	extra_args = { "--plugin-search-dir=" .. root },
 
-			vim.api.nvim_create_autocmd("BufWritePre", {
-				pattern = { "*.tsx", "*.jsx", "*.html" },
-				callback = function()
-					vim.lsp.buf.format({
-						async = false,
-						filter = function(client)
-							return client.name == "null-ls"
-						end,
-					})
-				end,
-			})
-		end,
-	},
+            extra_args = { "--plugin-search-dir=." },
+            -- Optional: define the working directory (useful in monorepos)
+            cwd = function(params)
+              return utils.root_pattern(".git")(params.bufname)
+            end,
+            env = {
+              PRETTIER_LOG_LEVEL = "debug",
+            },
+          }),
+          require("none-ls.diagnostics.eslint_d"),
+        },
+      })
+      local formatFile = function()
+        vim.lsp.buf.format({
+          async = false,
+          filter = function(client)
+            return client.name == "null-ls"
+          end,
+        })
+      end
+
+      -- map("n", "<leader>fm", formatFile, {})
+      --      vim.keymap.set("n", "<leader>fm", formatFile, { desc = "Format file with null-ls" })
+      vim.keymap.set("n", "<leader>fm", formatFile, { noremap = true, silent = true, desc = "Format file with null-ls" })
+
+
+      vim.api.nvim_create_autocmd("BufWritePre", {
+        pattern = { "*.tsx", "*.jsx", "*.html" },
+        callback = function()
+          formatFile()
+        end,
+      })
+    end,
+  },
 }
